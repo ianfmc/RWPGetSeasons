9940de40ae581561a8fccff5cf001ade9deb5ecf
Ian McCallum <ianfmc@gmail.com>
2016-10-28 11:41:48 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+var AWS = require('aws-sdk');
+
+exports.handler = function(event, context, callback) {
+    
+	var docClient = new AWS.DynamoDB.DocumentClient();
+    var params = {
+        TableName : 'Season'
+    };
+    docClient.scan(params, context, function(err, data) {
+    	if (err) {
+    		callback(err);
+    	}
+    	else (err) {
+    		callback(null, data);
+    	}
+    });
+};
